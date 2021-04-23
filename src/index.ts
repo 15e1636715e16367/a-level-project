35362373682f1b5cf31e4e58ed531272e2a88ecd
Tom H <15e16367@marling.school>
2021-04-23 12:18:53 +0100

    basic p5 working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import * as _ from 'lodash';
+import * as p5 from "p5"
 
-  function component() {
-    const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-  }
-
-  document.body.appendChild(component());
+let sketch = function(p: p5) {
+    let x = 100;
+    let y = 100;
+  
+    p.setup = function() {
+      p.createCanvas(700, 410);
+    };
+  
+    p.draw = function() {
+      p.background(0);
+      p.fill(255);
+      p.rect(x, y, 50, 50);
+    };
+  };
+  
+  let myp5 = new p5(sketch);
+  
