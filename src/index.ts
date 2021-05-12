e96154d6c0ea0664852de8f2fa2eee6e203f47fe
Tom H <15e16367@marling.school>
2021-05-12 09:57:11 +0100

    rewrote game / changed appearance

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,84 @@
 import * as p5 from "p5"
 
-let sketch = function (p: p5) {
-  let x = 400;
-  let y = 400;
 
+let stage = 0; 
+
+
+
+let plX = 400;
+let plY = 375;
+let pWidth = 30;
+let pHeight = 70;
+
+
+let blX = 200; 
+let blY = 300;
+let bWidth = 200;
+let bHeight = 40;
+
+
+
+
+let gametest = function (p : p5) {
   p.setup = function () {
-    p.createCanvas(700, 410);
-  };
+    p.createCanvas(800, 500);
+    p.rectMode(p.CENTER);
+    p.textAlign(p.CENTER);
+
+
+  } 
+
+
+
+
+  
 
   p.draw = function () {
-    p.background(220);
-    p.fill(0);
-    p.ellipse(x, y, 50, 50);
+
+    if(stage == 0) {
+      game();
+    }
+    
 
 
-    if (p.keyIsDown(68)) {
-      x += 5;
-    }
-    if (p.keyIsDown(65)) {
-      x -= 5;
-    }
-    if (p.keyIsDown(87)) {
-      y -= 5;
-    }
-    if (p.keyIsDown(83)) {
-      y += 5;
-    }
+
+  } 
+
+
+  function game() {
+    p.background(150, 230, 240);
+
+    p.noStroke();
+    p.fill(100, 200, 75);
+    p.rect(p.width/2, 450, p.width, 100)
+
+    p.noFill();
+    p.stroke(0);
+    p.strokeWeight(15);
+    p.rect(p.width/2, p.height/2, p.width, p.height);
+
+
+    p.stroke(0);
+    p.strokeWeight(5);
+    p.fill(255, 120, 0);
+    p.rect(blX, blY, bWidth, bHeight);
+
+
+    p.stroke(0);
+    p.fill(150, 0, 70);
+    p.rect(plX, plY, pWidth, pHeight);
+
+
   }
+
+
 
 }
 
 
 
 
-let myp5 = new p5(sketch);
+
+
+
+let myp5 = new p5(gametest)
